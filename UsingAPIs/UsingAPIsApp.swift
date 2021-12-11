4148d959e65da3d04c63aea571afe73b8a06f086
kirabin <kirabin.one@gmail.com>
2021-12-11 12:49:27 +0300

    Initial Commit

---
+++ UsingAPIs/UsingAPIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingAPIsApp.swift
+//  UsingAPIs
+//
+//  Created by kirabin on 11.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct UsingAPIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
